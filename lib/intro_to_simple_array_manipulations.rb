55a656340006dc4a36b106ea9696457af20dc4fb
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-21 00:38:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+def using_insert(array, element)
+  array.insert(4, element)
+end
+def using_uniq(array)
+  array.uniq
+end
+ def using_flatten(array[array])
+   array.flatten
+ end
